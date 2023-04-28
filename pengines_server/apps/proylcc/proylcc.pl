9ee2bd57a1eaf450488e45f96292bea5a27aaaf4
Aldana Ruppel <aldanaruppel.96@hotmail.com>
2023-04-28 16:48:09 -0300

    Iniciando primera etapa en Prolog

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -14,3 +14,21 @@ join(Grid, _NumOfColumns, _Path, RGrids):-
 	Grid = [N | Ns],	% La implementación actual es simplemente a modo de muestra, y no tiene sentido, debe reepmplazarla
 	N2 is N * 2,		% por una implementación válida.
 	RGrids = [[0 | Ns], [N2 | Ns]].
+
+%Funcion Obtener
+%Caso base :
+ObtenerIndice([X |Xs],Num) :- Num is 	(X*5)+Xs
+%Caso Base: La lista de listas de posiciones esta vacia.
+FuncionOrdenar([[]],Col,[R |Rs],[])
+% 1) Caso recursivo :
+FuncionOrdenar([X|Xs],Col,[],_listaVacia) :-
+	FuncionOrdenar([X|Xs],Col,[Pos]) Pos is  
+
+
+%1 Etapa de la funcionalidad del juego)
+%EliminandoBloques(Grid ,Columnas , Camino , Retorno)
+%Caso base 1 :
+EliminandoBloques([E] ,Col,_Path,_Retorno)
+%Caso Recursivo :
+EliminandoBloques([E |Es],Col,[[P|Ps]],[R |Rs]) :- 
+	EliminandoBloques([Es] ,Col,[[Ps]],[R |Rs]) ,
