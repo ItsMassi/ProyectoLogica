459d8bce0809b5205595e64e38f8576b08ced9d8
Aldana Ruppel <aldanaruppel.96@hotmail.com>
2023-05-02 20:54:36 -0300

    Eliminando codigo basura

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -1,5 +1,7 @@
 :- module(proylcc, [
-		join/4
+		join/4,
+        obtenerIndice/2,
+        eliminandoBloquesB/4
 		
 
 
@@ -12,12 +14,13 @@
  * en la grilla Grid, con número de columnas NumOfColumns. El número 0 representa que la celda está vacía. 
  */ 
 
-join(Grid, _NumOfColumns, Path, RGrids):-
-	eliminandoBloques(Path,Grid, RGrids).
+join(Grid, Col, Path, RGrids):-
+	funcionOrdenar(Grid,Col,Path,L),
+    eliminandoBloquesBShell(L,Grid,RGrids).
 
 obtenerIndice([X|Xs], Num) :- Num is (X*5) + Xs.
 
-funcionOrdenar([X|Xs], Col, P, L) :-
+funcionOrdenar([X|Xs], Col, P, L) :- %L es la Lista resultante
 	obtenerIndice(X, Pos),
 	append(P, [Pos], Q),
 	funcionOrdenar(Xs, Col, Q, NewL),
@@ -25,7 +28,6 @@ funcionOrdenar([X|Xs], Col, P, L) :-
 	
 funcionOrdenar([], Col, P, L) :-
 	sort(P, NewL),
-	write(NewL),
 	L = NewL.
 	
 %1 Etapa de la funcionalidad del juego).
@@ -34,27 +36,20 @@ eliminandoBloquesBShell(L, T, Copia) :-
     eliminandoBloquesB(L, T, [], Copia, 0).
 
 eliminandoBloquesB([], T, Acc, Copia, _) :-
-    write('caso base 1'), 
+ 
     append(Acc, [], Copia).
 
 eliminandoBloquesB([Cont], T, Acc, Copia, Cont) :-
-    write('caso base 2'),
-    write(Cont),
     append(Acc, [1], NewAcc),
     ContAux is Cont + 1,
     eliminandoBloquesB([], T, NewAcc, Copia, ContAux).
 
 eliminandoBloquesB([Cont|Ls], [H|T], Acc, Copia, Cont) :-
-    write('Recursivo 1'),
-    write(Cont),
     append(Acc, [0], NewAcc),
     ContAux is Cont + 1,
     eliminandoBloquesB(Ls, T, NewAcc, Copia, ContAux).
 
 eliminandoBloquesB(L, [H|T], Acc, Copia, Cont) :-
-    write('Recursivo 2'),
-    write(Cont),
-    write(L),
     append(Acc, [H], NewAcc),
     ContAux is Cont + 1,
     eliminandoBloquesB(L, T, NewAcc, Copia, ContAux).
